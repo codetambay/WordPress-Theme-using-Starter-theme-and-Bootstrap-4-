a5409aab4428f0fd487f1fb17f6fbf5568710556
codetambay <40927174+codetambay@users.noreply.github.com>
2020-01-28 16:22:27 +0800

    video 3 files
    video 3 files

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+/* 
+    Author      : baymax
+    Date        : Jan 2020
+    Url         : https://codetambay.com/
+    Github      : 
+    File Name   : custom.css
+    Description : WordPress Theme, Responsive, Bootstrap 4, Starter Theme
+    Version     : 0.1
+*/
+(function($){
+
+})(jQuery);
